c9b5cd2e64a50eae21ace3d25bee04868fc4ecfe
xZodiac2 <xirenoa@gmail.com>
2023-11-27 14:55:24 +0400

    refactoring

--- core/src/main/java/com/ilya/core/UseCase.kt
+++
@@ -1,5 +0,0 @@
-package com.ilya.core
-
-interface UseCase<in R, out T> {
-    suspend operator fun invoke(data: R): Result<T>
-}
--- data/src/main/java/com/ilya/data/DataError.kt
+++
@@ -1,6 +0,0 @@
-package com.ilya.data
-
-sealed class DataError(override val message: String? = null) : Throwable(message) {
-    object FavouriteAlreadyExist : DataError()
-    object FavouriteNotFound : DataError()
-}
